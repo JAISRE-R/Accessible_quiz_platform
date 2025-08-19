d5a039845e78478eba216981af12f011eff7a9b8
JAISRE R <jaiiiiiisre@gmail.com>
2025-08-19 20:47:53 +0530

    Initial commit - Spring Boot project setup

---
+++ accessiblequiz/src/main/java/com/quizapp/accessiblequiz/AccessiblequizApplication.java
@@ -0,0 +1,13 @@
+package com.quizapp.accessiblequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessiblequizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessiblequizApplication.class, args);
+	}
+
+}
