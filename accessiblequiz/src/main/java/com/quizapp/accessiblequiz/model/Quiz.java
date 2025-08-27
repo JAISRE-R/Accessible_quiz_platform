700baf33ad7e1b3d94f7624c861f6c87e95569bc
JAISRE R <jaiiiiiisre@gmail.com>
2025-08-27 22:07:41 +0530

    Added Quiz entity and repository

---
+++ accessiblequiz/src/main/java/com/quizapp/accessiblequiz/model/Quiz.java
@@ -0,0 +1,33 @@
+package com.quizapp.accessiblequiz.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quizzes")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String difficulty;
+
+    // Getters & Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getDifficulty() { return difficulty; }
+    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
+}
+
---
+++ accessiblequiz/src/main/java/com/quizapp/accessiblequiz/repository/QuizRepository.java
@@ -0,0 +1,7 @@
+package com.quizapp.accessiblequiz.repository;
+
+import com.quizapp.accessiblequiz.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+}
